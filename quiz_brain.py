d77031a1b532bd3953388e817c7ca7e3b8e2a528
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-08-01 21:46:02 +0530

    Use  convert the ascii string into html script by replacing ascii characters with special characters by using html, and directly return the output instead of printing.

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,7 +1,4 @@
-from data import question_data
-from question_model import Questions
-
-
+import html
 class QuizBrain:
     def __init__(self, qlist):
         self.user_answer = None
@@ -16,16 +13,13 @@ class QuizBrain:
     def next_question(self, ):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        self.user_answer = input(f"Q.{self.question_number}: {current_question.question}  (True/False): ")
-        self.check_question(self.user_answer, current_question.correct_answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
 
     def check_question(self, user_answer, correct_answer):
 
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-        print(f"The correct answer was: {correct_answer}.")
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
