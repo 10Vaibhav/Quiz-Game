17ec01c7ed4084bcfe9b0dade15214cabdd50ff7
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2024-08-01 21:40:17 +0530

    Create UI object

--- main.py
+++ main.py
@@ -11,9 +11,4 @@ for que in question_data:
     question_bank.append(new_q)
 
 quiz = QuizBrain(question_bank)
-
-while quiz.still_has_question():
-    quiz.next_question()
-
-print("You've completed the quiz.")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+quiz_ui = QuizInterface(quiz)
